e138491823a5b5609a2417d8cd341481b5198cdd
Chris Lorenzo <chiefcll@gmail.com>
2024-11-05 20:29:57 -0500

    update solid tests with different versions

--- frameworks/solid-perf-test/src/index.jsx
+++ frameworks/solid-perf-test/src/index.jsx
@@ -1,15 +1,18 @@
-import { createRenderer, Config } from '@lightningtv/solid';
-import HelloWorld from './pages/HelloWorld';
-import { WebGlCoreRenderer } from '@lightningjs/renderer/webgl';
-// import TextPage from './pages/Text';
-// import CounterPage from './pages/Counter';
-// import ButtonsPage from './pages/ButtonsPage';
-// import NotFound from './pages/NotFound';
+import { createRenderer, Config } from "@lightningtv/solid";
+import { HashRouter } from "@lightningtv/solid/primitives";
+import { Route } from "@solidjs/router";
+import DirectUpdate from "./pages/DirectUpdate";
+import TilePage from "./pages/Tile";
+import MapPage from "./pages/Map";
+import IndexPage from "./pages/Index";
+import ForPage from "./pages/For";
+import { WebGlCoreRenderer } from "@lightningjs/renderer/webgl";
+import App from "./pages/App";
 
 //import coreExtensionModuleUrl from './AppCoreExtensions.js?importChunkUrl';
 
 Config.debug = false;
-Config.fontSettings.fontFamily = 'Ubuntu';
+Config.fontSettings.fontFamily = "Ubuntu";
 Config.fontSettings.color = 0xffffffff;
 Config.rendererOptions = {
   //coreExtensionModule: coreExtensionModuleUrl,
@@ -20,7 +23,13 @@ Config.rendererOptions = {
   // deviceLogicalPixelRatio: 1
 };
 
-const {render} = createRenderer();
-render(() =>  (
-  <HelloWorld />
+const { render } = createRenderer();
+render(() => (
+  <HashRouter root={(props) => <App {...props} />}>
+    <Route path="" component={DirectUpdate} />
+    <Route path="tile" component={TilePage} />
+    <Route path="for" component={ForPage} />
+    <Route path="map" component={MapPage} />
+    <Route path="index" component={IndexPage} />
+  </HashRouter>
 ));
--- frameworks/solid-perf-test/src/pages/App.jsx
+++ frameworks/solid-perf-test/src/pages/App.jsx
@@ -2,38 +2,26 @@ import { View } from "@lightningtv/solid";
 import { useFocusManager } from "@lightningtv/solid/primitives";
 
 const App = (props) => {
-  useFocusManager({
-    Announcer: ["a"],
-    Menu: ["m"],
-    PressT: 't',
-    Counter: 'c',
-    Buttons: 'b',
-    Escape: ["Escape", 27],
-    Backspace: ["Backspace", 8],
-    Left: ["ArrowLeft", 37],
-    Right: ["ArrowRight", 39],
-    Up: ["ArrowUp", 38],
-    Down: ["ArrowDown", 40],
-    Enter: ["Enter", 13],
-  });
-  
+  useFocusManager();
+
   //const navigate = useNavigate();
   // const announcer = useAnnouncer();
   // announcer.debug = false;
   // announcer.enabled = false;
 
   return (
-    <View ref={window.APP}
+    <View
+      ref={window.APP}
       // onAnnouncer={() => announcer.enabled = !announcer.enabled}
       onLast={() => history.back()}
-      onText={() => navigate('/text')}
-      onCounter={() => navigate('/counter')}
-      onButtons={() => navigate('/buttons')}
-      onMenu={() => navigate('/')}>
-        <View color={0x071423ff} />
-        {props.children}
+      onText={() => navigate("/text")}
+      onCounter={() => navigate("/counter")}
+      onButtons={() => navigate("/buttons")}
+      onMenu={() => navigate("/")}
+    >
+      {props.children}
     </View>
-  )
+  );
 };
 
 export default App;
