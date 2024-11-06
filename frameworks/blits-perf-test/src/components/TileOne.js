35c22f3a989e2379391cef1c360de28c23012f26
Chris Lorenzo <chiefcll@gmail.com>
2024-11-05 21:10:59 -0500

    fix missing link - not reactive though

--- frameworks/blits-perf-test/src/components/TileOne.js
+++ frameworks/blits-perf-test/src/components/TileOne.js
@@ -2,7 +2,7 @@ import Blits from "@lightningjs/blits";
 
 export default Blits.Component("TileOne", {
   template: `
-    <Element :y="$item.y" :x="$item.x" :w="$item.w" :h="$item.h" :color="$item.color" :effects="[{type: 'radius', props: {radius: $item.radius}}]" />
+    <Element :y="$item.y" :x="$item.x" :w="$item.width" :h="$item.height" :color="$item.color" :effects="[{type: 'radius', props: {radius: $item.radius}}]" />
   `,
   props: ["item"],
 });
